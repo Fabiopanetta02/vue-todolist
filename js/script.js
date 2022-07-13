ac3fa2a3c685837825d2b61bf5866a981034da1c
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-07-13 15:08:24 +0200

    Added MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -2,20 +2,27 @@ const root = new Vue({
     name: 'Todolist',
     el:'#root',
     data: {
-       tasks: [
-        {
-            text:'Fare la spesa',
-            done: true,
-        },
-        {
-            text:'Pulire casa',
-            done: false,
-        },
-       ] 
+        newTask: '',
+        tasks: [
+            {
+                text:'Fare la spesa',
+                done: true,
+            },
+            {
+                text:'Pulire casa',
+                done: false,
+            },
+        ] 
     },
     methods: {
         removeTask(index) {
             this.tasks.splice(index, 1);
+        },
+        addTask() {
+            this.tasks.push({
+                text: this.newTask,
+                done: true,
+            });
         }
     }
 })
